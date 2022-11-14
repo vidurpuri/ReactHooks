a427e7a1e65a02485868e8afca8036d0e23a63ca
vidurpuri <31599019+vidurpuri@users.noreply.github.com>
2022-11-14 11:02:31 -0800

    UseState Created UseState hook

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import "./styles.css";
-import Reducer from "./useReducer";
+import UseState from "./useState";
 
 export default function App() {
   return (
     <div className="App">
-      <Reducer />
+      <UseState />
     </div>
   );
 }
---
+++ src/useState.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+
+const UseState = () => {
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState(true);
+
+  return (
+    <div>
+      <div>{count}</div>
+      <div>
+        <button
+          type="submit"
+          onClick={() => {
+            setCount(count + 1);
+            setText(!text);
+          }}
+        >
+          Increment
+        </button>
+      </div>
+      <div>{text && <p>This is a text</p>}</div>
+    </div>
+  );
+};
+
+export default UseState;
